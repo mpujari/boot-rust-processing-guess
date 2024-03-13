76eb4fc03424a4330904e54db6f8672af58c8533
mpujari <mpujari@users.noreply.github.com>
2024-03-13 12:08:28 +0530

    ch02 guessing game with match and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 
 // Learning Rust from https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 
@@ -7,7 +7,7 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     println!("The secrete number is: {secret_number}");
 
@@ -19,5 +19,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line.");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Less => println!("Too small!")
+    }
 }
