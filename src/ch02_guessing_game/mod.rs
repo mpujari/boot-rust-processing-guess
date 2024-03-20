2dcaf51f9bfff3b1eff5fa241dbcd319cea14824
mpujari <mpujari@users.noreply.github.com>
2024-03-20 10:36:58 +0530

    ch02 refactored the code to go with modules

---
+++ src/ch02_guessing_game/mod.rs
@@ -0,0 +1,39 @@
+// Learning Rust from https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+pub mod ch02_guessing_game_mod {
+
+    use std::{cmp::Ordering, io};
+    use rand::Rng;
+
+    pub fn guess_game() {
+        println!("Guess the number!");
+        let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+        // println!("The secrete number is: {secret_number}");
+        loop {
+            println!("Please input your guess");
+
+            let mut guess = String::new();
+
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line.");
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue
+            };
+
+            println!("You guessed: {guess}");
+
+            match guess.cmp(&secret_number) {
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Less => println!("Too small!")
+            }
+        }
+    }
+
+}
+
